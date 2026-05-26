8b67adfccd44a37a12f8c315d497da19b4cab07b
nevons <tyaginavanshpe@gmail.com>
2026-05-26 11:08:59 +0530

    1.15
    added pickup sounds

--- scripts/interactable.gd
+++ scripts/interactable.gd
@@ -6,10 +6,24 @@ signal interacted(item_type)
 
 @export var item_name: String = "Item"
 @export var item_enum_id: int = 0 # Matches the ItemManager's EquippedItem enum
+@onready var pickup_sound : AudioStreamPlayer3D = $pickup
 
 func prompt_text() -> String:
 	return "Press [E] to pick up " + item_name
 
 func interact():
 	emit_signal("interacted", item_enum_id)
+	
+	if pickup_sound and pickup_sound.stream:
+		for child in get_children():
+			if child is MeshInstance3D:
+				child.visible = false
+		
+		# Disable collisions so the player can't interact with it twice
+		monitorable = false
+		monitoring = false
+		
+		# Play the sound and wait until it's finished before deleting the node
+		pickup_sound.play()
+		await pickup_sound.finished
 	queue_free() # Remove the physical prop from the cabin world
